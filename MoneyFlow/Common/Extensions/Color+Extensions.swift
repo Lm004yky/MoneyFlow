84256162f53374788a309eb69ac4dd41fb179fd9
Lm004yky <albertov.sasha@bk.ru>
2025-10-20 10:34:19 +0500

    Add Design System with colors

---
+++ MoneyFlow/Common/Extensions/Color+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Color+Extensions.swift
+//  MoneyFlow
+//
+//  Created by Ykylas Nurkhan on 20.10.2025.
+//
+
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let primary = Color("Primary")
+    let secondary = Color("Secondary")
+    let danger = Color("Danger")
+    let success = Color("Success")
+    let warning = Color("Warning")
+    let background = Color("Background")
+    let textPrimary = Color("TextPrimary")
+    let textSecondary = Color("TextSecondary")
+}
